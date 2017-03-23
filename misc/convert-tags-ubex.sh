75be03368ad8a835cd6b2c43305ef9d4588bc4c4
Tigran Aivazian <aivazian.tigran@gmail.com>
2017-03-23 15:05:04 +0000

    Scripts for generating UBEX text

---
+++ misc/convert-tags-ubex.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#
+# Convert the following: tags, emdashes.
+# 
+# {{{br}}} -> \\
+# {i{i{...}i}i} -> \bibemph{...}
+# {b{b{...}b}b} -> \bibtextbf{...}
+# {u{u{...}u}u} -> \bibtextul{...}
+# {r{r{...}r}r} -> \textcolor{ubdarkred}{...}
+# "...Number" -> "\bibdf Number" (more than one dot means it is a leader, "dot filler")
+# "...Letter" -> "\ldots Letter"
+# "-" -> "\hyp{}"
+# " —" -> "~---"
+# "[0-9a-zA-Z]. Text" -> "[0-9a-zA-Z].~Text" (unbreakable space)
+# "" -> "" (Get rid of MS-DOS ^M character at the end of lines)
+# "<FEFF>" -> "" (Get rid of Unicode marker at the beginning of file).
+#
+
+INPUT_DIR=$(pwd)/u8
+OUTPUT_DIR=$(pwd)/u8-notags
+
+rm -rf ${OUTPUT_DIR} ; mkdir -p ${OUTPUT_DIR}
+
+cd ${INPUT_DIR}
+list=$(find -name '*.u8')
+cd ${OUTPUT_DIR}
+for path in $list
+do
+   dir=$(dirname $path)
+   file=$(basename $path)
+   mkdir -p $dir
+   sed -e "s%{{{br}}}%\\\\\\\%g" \
+       -e "s%{i{i{%<em>%g" \
+       -e "s%}i}i}%</em>%g" \
+       -e "s%  *% %g" ${INPUT_DIR}/${path} | \
+       sed -e "s%%%g" \
+           -e "s% $%%g" \
+           -e "s%﻿%%g" > $dir/$file
+done
---
+++ misc/mkubex.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+#
+# Generate html source files for Urantia Book Explorer from UBDS exemplar and metric files.
+#
+
+METRIC_FILE=metrics/exemplarmap.js
+DATA_DIR=u8-notags
+FILE_PREFIX=rusnys2006_exemplar
+PARCLUSTER_FILE=StandardizedParagraphBreaks.txt
+PAPER_AUTHORS=paper-authors.txt
+OUT=ubex
+TOC=$OUT/toc.html
+
+declare -a parlist lineid linetype
+declare -i sec i paridx linenum
+
+./convert-tags-ubex.sh
+
+rm -rf $OUT ; mkdir -p $OUT
+echo -E "<ul class='toc' id='toc4'>" > $TOC
+echo -E "<li title='<i>Представленно при содействии отряда личностей сверхвселенной, находящихся на Уверсе, дейсвующего под руководством Древних Дней Орвонтона.</i>'><a href=\".U0_0_1\"><b>I: ЦЕНТРАЛЬНАЯ ВСЕЛЕННАЯ И СВЕРХВСЕЛЕННЫЕ</b></a>" >> $TOC
+echo -E " <ul>" >> $TOC
+
+for ((i = 0; i <= 196; i++));
+do
+   if ((i == 32)) ; then
+      echo -E " </ul>" >> $TOC
+      echo -E "</li>" >> $TOC
+      echo -E "<li title='<i>Под покровительством Небадонского Отряда Личностей Локальной Вселенной, действующих на основе полномочий от Гавриила из Спасограда.</i>'><a href=\".U32_0_1\"><b>II: ЛОКАЛЬНАЯ ВСЕЛЕННАЯ</b></a>" >> $TOC
+      echo -E " <ul>" >> $TOC
+   fi
+   if ((i == 57)) ; then
+      echo -E " </ul>" >> $TOC
+      echo -E "</li>" >> $TOC
+      echo -E "<li title='<i>Материалы этой части подготовлены Отрядом Личностей локальной вселенной, действующих по распоряжению Гавриила из Спасограда.</i>'><a href=\".U57_0_1\"><b>III: ИСТОРИЯ УРАНТИИ</b></a>" >> $TOC
+      echo -E " <ul>" >> $TOC
+   fi
+   if ((i == 120)) ; then
+      echo -E " </ul>" >> $TOC
+      echo -E "</li>" >> $TOC
+      echo -E "<li title='<i>Материалы этой части подготовлены комиссией из 12 срединников Урантии, работавшей под началом Мелхиседека, руководителя комиссии по откровениям.<br>Основные данные о событиях сообщены срединником второго рода, который некогда был назначен надчеловеческим хранителем апостола Андрея.</i>'><a href=\".U120_0_1\"><b>IV: ЖИЗНЬ И УЧЕНИЯ ИИСУСА</b></a>" >> $TOC
+      echo -E " <ul>" >> $TOC
+   fi
+   p=$(printf "%03d" $i)
+   parlist=($(sed -ne "s%^/\* Paper${i} \*/  *\[\(.*\)\],*%\1%p" ${METRIC_FILE} | sed -e "s%,%%g"))
+   numsec=${#parlist[@]}
+   echo "paper $p: ($numsec sections)"
+   linenum=0
+   lineid=()
+   linetype=()
+   for ((sec = 0; sec < $numsec; sec++));
+   do
+      for ((paridx = 0; paridx <= parlist[$sec]; paridx++));
+      do
+         if ((i == 0 && sec == 12 && paridx == 10)) ; then
+            linetype[$linenum]="sectiontitle"
+         fi
+         if ((paridx == 0)) ; then
+            linetype[$linenum]="sectiontitle"
+            if ((sec == 0)) ; then
+               linetype[$linenum]="papertitle"
+            fi
+         fi
+         if ((i == 139 && sec > 9)) ; then
+            lineid[$linenum]="p$p $((sec+1)):${paridx}"
+         else
+            lineid[$linenum]="p$p ${sec}:${paridx}"
+         fi
+         ((linenum++))
+      done
+   done
+   linenum=0
+   >$OUT/p${p}.html
+   while read -r line
+   do
+      text=$(echo -E "$line" | sed -e "s%^\[[0-9][0-9]*\]| *%%")
+      chap=$(echo "${lineid[$linenum]}" | sed -ne "s%p... \([0-9][0-9]*\):[0-9][0-9]*%\1%p")
+      verse=$(echo "${lineid[$linenum]}" | sed -ne "s%p... [0-9][0-9]*:\([0-9][0-9]*\)%\1%p")
+      if [ "${linetype[$linenum]}" = "papertitle" ] ; then
+         papertitle=$(echo -E "$text" | sed -ne "s%^Текст [0-9][0-9]*.. \(.*\)$*$%\1%p")
+         author=$(grep "${p}" ${PAPER_AUTHORS} | sed 's/^p...://')
+         echo -E "    </ul>" >> $OUT/toc.html
+         echo -E "  </li>" >> $OUT/toc.html
+         if ((i == 0)) ; then
+            echo -E "  <li title='<i>$author</i>'><a class="U${i}_${chap}_1" href=".U${i}_${chap}_1"><b>Предисловие</b></a>" >> $OUT/toc.html
+         else
+            echo -E "  <li title='<i>$author</i>'><a class="U${i}_${chap}_1" href=".U${i}_${chap}_1"><b>$i. $papertitle</b></a>" >> $OUT/toc.html
+         fi
+         echo -E "    <ul>" >> $OUT/toc.html
+      else
+         if [ "${linetype[$linenum]}" = "sectiontitle" ] ; then
+            echo -E "      <li><a class="U${i}_${chap}_${verse}" href=".U${i}_${chap}_${verse}">$text</a></li>" >> $OUT/toc.html
+            echo -E "<h4><a class="U${i}_${chap}_${verse}" href=".U${i}_${chap}_${verse}">$text</a></h4>" >> $OUT/p${p}.html
+         else
+            if grep -q " $i:$chap.$((verse-1))$" ${PARCLUSTER_FILE}
+            then
+               echo -E "<p><a class="U${i}_${chap}_${verse}" href=".U${i}_${chap}_${verse}"><sup>${i}:${chap}.${verse}</sup></a> ¶ ${text}" >> $OUT/p${p}.html
+            else
+               echo -E "<p><a class="U${i}_${chap}_${verse}" href=".U${i}_${chap}_${verse}"><sup>${i}:${chap}.${verse}</sup></a> ${text}" >> $OUT/p${p}.html
+            fi
+         fi
+      fi
+      ((linenum++))
+   done < ${DATA_DIR}/${FILE_PREFIX}_p${p}.u8
+done
+echo -E " </ul>" >> $TOC
+echo -E "</li>" >> $TOC
+echo -E "</ul>" >> $TOC
